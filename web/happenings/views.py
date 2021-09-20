18bbd0d21544d76483bcd3d761f565cf8cdd1ade
sigtheidiot <sigridge@stud.ntnu.no>
2021-09-20 13:24:24 +0200

    #5 - Show a random event, also added views for spesific events and lists of all events

--- web/happenings/views.py
+++ web/happenings/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from random import randint
+from django.views.generic import DetailView, ListView
+from .models import Schedule
 
-# Create your views here.
+
+class EventList(ListView):
+    model = Schedule
+    template_name = "event_list_view.html"
+    context_object_name = "scheduled_events_list"
+
+
+class EventView(DetailView):
+    model = Schedule
+    template_name = "event_detail_view.html"
+    context_object_name = 'scheduled_event'
+
+
+class RandomEventView(DetailView):
+    queryset = Schedule.objects.all()
+    template_name = "event_detail_view.html"
+    context_object_name = 'scheduled_event'
+
+    def get_object(self):
+        object_list = Schedule.objects.all()
+        number = randint(1, len(object_list))
+        return Schedule.objects.get(pk=number)
+
