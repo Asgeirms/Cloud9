a4a364c11a7acc7fbcf153c75029396bef90e1f4
henrfj <henrfj@stud.ntnu.no>
2021-09-21 13:02:55 +0200

    Removed 'admin-button' from new event form

--- web/happenings/urls.py
+++ web/happenings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DisplayEventsView, AddNewEvent
 
 urlpatterns = [
-    path('', DisplayEventsView.as_view(), name='all-events'),
-    path('create/', AddNewEvent.as_view(), name="create-event"),
+    path('', DisplayEventsView.as_view(), name='all_events'),
+    path('create/', AddNewEvent.as_view(), name='create_event'),
 ]
--- web/happenings/views.py
+++ web/happenings/views.py
@@ -1,9 +1,20 @@
+from django.db.models import fields
+from django.db.models.base import Model
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
+from django.forms import ModelForm
+
 from .models import Event
+
+# Formclass. Can be used to both add new and to alter existing.
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name', 'location', 'min_price', 'max_price', 'description']
+        #fields = '__all__'
 
 # Create your views here.
 class DisplayEventsView(TemplateView):
@@ -12,10 +23,8 @@ class DisplayEventsView(TemplateView):
 class AddNewEvent(CreateView):
     template_name = "happenings/add_event.html"
     models = Event
-    fields = '__all__'
+    form_class = EventForm
     context_object_name = "event"
     success_url = reverse_lazy('all-events')
 
-    def get_queryset(self):
-        return Event.objects.all()
 
