41e1437f9679da6550595e103413ea9d5a9043c9
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-22 13:21:50 +0200

    login works

--- web/web/settings.py
+++ web/web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authenticate',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'web.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
