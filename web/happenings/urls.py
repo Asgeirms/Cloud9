9a78ed9e6ffbd1c576cea2e33ee10b4ba7e7b558
maisergodt <maihtran.99@gmail.com>
2021-09-24 12:48:05 +0200

    #4 redirect to landing page

--- web/happenings/urls.py
+++ web/happenings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import DisplayEventsView, AddEventView, EventList, RandomEventView, EventView
 
 urlpatterns = [
-    path('', EventList.as_view(), name="events"),
+    path('', EventList.as_view(), name="all_events"),
     path('<int:pk>', EventView.as_view(), name="events"),
     path('random', RandomEventView.as_view(), name='spesific_event'),
     path('create/', AddEventView.as_view(), name='create_event'),
