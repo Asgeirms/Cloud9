711b1b9b12fbc899bfb93d4b45ca6bd21af51693
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-26 22:52:46 +0200

    Capitalized names and added tests

--- web/authenticate/forms.py
+++ web/authenticate/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# Form for the user registration page
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(label='First Name')
@@ -10,3 +11,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+    # Capitalizes the first name
+    def clean_first_name(self):
+        return self.cleaned_data['first_name'].capitalize()
+
+    # Capitalizes the last name
+    def clean_last_name(self):
+        return self.cleaned_data['last_name'].capitalize()
--- web/authenticate/tests.py
+++ web/authenticate/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .forms import UserRegisterForm
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserRegisterFormTests(TestCase):
+    def test_create_user_success(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertTrue(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_missing_username(self):
+        form_data = {"first_name": "test", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+    
+    def test_create_user_missing_first_name(self):
+        form_data = {"username": "NewTestUser", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_missing_last_name(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_missing_email(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_missing_password1(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "email": "testing@test.no", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_missing_password2(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_create_user_password_not_matching(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest12345"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertFalse(User.objects.filter(username = "NewTestUser").exists())
+
+    def test_uppercase_names(self):
+        form_data = {"username": "NewTestUser", "first_name": "test", "last_name": "user", "email": "testing@test.no", "password1": "passwordtest1234", "password2": "passwordtest1234"}
+        response = self.client.post("/authenticate/register/", data=form_data)
+        self.assertTrue(User.objects.filter(first_name = "Test").exists())
+        self.assertFalse(User.objects.filter(first_name = "test").exists())
+        self.assertTrue(User.objects.filter(last_name = "User").exists())
+        self.assertFalse(User.objects.filter(last_name = "user").exists())
