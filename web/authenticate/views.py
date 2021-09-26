dccea6c142b91b6c5820617b7effba22e90cf845
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-26 22:53:08 +0200

    comments

--- web/authenticate/views.py
+++ web/authenticate/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 
-# Create your views here.
+# Register page for users
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            # Makes user and redirects to login
             return redirect('login')
     else:
         form = UserRegisterForm()
+    # Renders a form on the page
     return render(request, 'register.html', {'form': form})
