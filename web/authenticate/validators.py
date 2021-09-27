00c4a5b062b248cdad52115cd84836d7439fc22b
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-27 09:10:24 +0200

    fixed logic error

--- web/authenticate/validators.py
+++ web/authenticate/validators.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
-def validate_only_letters(value):
+def validate_is_name(value):
     if not isinstance(value, str):
         raise ValidationError('Not a string')
-    if not value.isalpha():
-        raise ValidationError('Can only contain letters')
+    if not value.replace("-", "").replace(" ","").isalpha():
+        raise ValidationError('A name can only contain letters, hyphens and spaces')
+
