8bf15095b3f3cbd8a4b366832b8b6be5281fc1bd
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-27 11:16:56 +0200

    changed url name

--- web/web/urls.py
+++ web/web/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authenticate/', include('authenticate.urls'))
+    path('user/', include('authenticate.urls'))
 ]
