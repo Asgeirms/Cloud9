62fdc4d4c704f5f129342eabfd8c3654ae3d9a31
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-27 09:12:27 +0200

    comment

--- web/authenticate/validators.py
+++ web/authenticate/validators.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
+# Checks if string and if it contains only letters, hyphens and spaces
 def validate_is_name(value):
     if not isinstance(value, str):
         raise ValidationError('Not a string')
