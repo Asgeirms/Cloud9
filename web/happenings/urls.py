1525c94353e0810c0ae0cf9e1dd87bbc4f58c9d8
maisergodt <maihtran.99@gmail.com>
2021-09-29 16:48:57 +0200

    cleanup: refactor names and deleted unused file

--- web/happenings/urls.py
+++ web/happenings/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.EventListView.as_view(), name="events"),
-    path('me/create/', views.AddEventView.as_view(), name='create_event'),
+    path('me/create/', views.SuggestEventView.as_view(), name='create_event'),
     path('me/', views.MyEventsView.as_view(), name='my_events'),
-    path('me/<int:pk>', views.DetailedEventView.as_view(), name="detail"),
+    path('me/<int:pk>', views.EventDetailView.as_view(), name="event_detail"),
     path('<int:pk>', views.EventView.as_view(), name="events"),
     path('random', views.RandomEventView.as_view(), name='random_event'),
 ]   
