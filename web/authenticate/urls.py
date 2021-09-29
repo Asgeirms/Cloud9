091d24b3f86edbfb48b2855b66289699f2f976f8
Ignas Bråtelund <ignas.bratelund@gmail.com>
2021-09-29 17:41:10 +0200

    registerview is now inheriting from CreateView

--- web/authenticate/urls.py
+++ web/authenticate/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
-from .views import RegisterView
+from .views import RegisterCreateView
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='authenticate/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='authenticate/logout.html'), name='logout'),
-    path('register/', RegisterView, name='register')
+    path('register/', RegisterCreateView.as_view(), name='register')
 ]
--- web/authenticate/views.py
+++ web/authenticate/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
+from .models import User
+from django.urls import reverse_lazy
 
 # Register page for users
-def RegisterView(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Makes user and redirects to login
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    # Renders a form on the page
-    return render(request, 'authenticate/register.html', {'form': form})
+class RegisterCreateView(CreateView):
+    template_name = "authenticate/register.html"
+    models = User
+    form_class = UserRegisterForm
+    context_object_name = "user"
+    success_url = reverse_lazy('login')
