5460240e51b1e2a27f152eb92181ba7fbbf9dee4
maisergodt <maihtran.99@gmail.com>
2021-10-01 18:20:25 +0200

    #12 adding datetime formatter in calendar widget

--- web/happenings/forms.py
+++ web/happenings/forms.py
@@ -17,6 +17,7 @@ class EventForm(forms.ModelForm):
         'end_time'
         'description',
     ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['start_time'].widget.attrs.update({'autocomplete': 'off'})
@@ -30,20 +31,35 @@ class EventForm(forms.ModelForm):
             'max_price': 'Maximum price'
         }
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Your name'}),
+            'name': forms.TextInput(attrs={'placeholder': 'Name of the event'}),
             'location': forms.TextInput(attrs={'placeholder': 'Where to host?'}),
             'description': forms.Textarea(attrs={'placeholder': 'Describe your event!'}),
         }
-    
-    def save(self, commit=True):
-        """Coupling Schedule and Event. """
-        event = super().save(commit=commit)
-        event_schedule = Schedule.objects.create(
-            start_time=self.cleaned_data['start_time'],
-            end_time=self.cleaned_data['end_time'],
-            event=event
-        )
-        event.save()
 
-        return event
+        def clean(self):
+            cleaned_data = super().clean()
+            start_time = cleaned_data.get('start_time')
+            end_time = cleaned_data.get('end_time')
 
+            # Validating the time fields
+            if start_time > end_time:
+                self.add_error(
+                    'start_time',
+                    forms.ValidationError("Start time cannot begin after end time!"),
+                    code='invalid_time'
+                )
+            return cleaned_data
+
+        def save(self, commit=True):
+            """Coupling Schedule and Event. """
+
+            # Happens when user is submitting the form.
+            # Need to save the event first
+            event = super().save(commit=commit)
+
+            # Linking the saved event to a schedule object
+            event_schedule = Schedule.objects.create(
+                start_time=self.cleaned_data['start_time'],
+                end_time=self.cleaned_data['end_time'],
+                event=event
+            )
