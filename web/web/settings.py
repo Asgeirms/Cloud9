041a1cc4239b23703bf787d4767e212aacaf0be2
maisergodt <maihtran.99@gmail.com>
2021-10-07 17:22:22 +0200

    #31 init swiping app

--- web/web/settings.py
+++ web/web/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'authenticate',
     'happenings',
     'web',
+    'swiping',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authenticate.User'
 
 LOGIN_REDIRECT_URL = 'events'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
