0e3268f41ba211dc0724146d4785cbbc0d696c3e
maisergodt <maihtran.99@gmail.com>
2021-10-08 10:54:22 +0200

    #31 Implemented swiping view w/ session storage

--- web/web/urls.py
+++ web/web/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('events/', include('happenings.urls')),
     path('admin/', admin.site.urls),
-    path('user/', include('authenticate.urls'))
+    path('user/', include('authenticate.urls')),
+    path('swiping/', include('swiping.urls')),
 ]
