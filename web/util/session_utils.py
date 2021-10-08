6e9d610fd1c8e84b604243e7f17279ee88dea9d8
maisergodt <maihtran.99@gmail.com>
2021-10-08 10:53:41 +0200

    #31 Add tests for session utils

--- web/util/session_utils.py
+++ web/util/session_utils.py
@@ -5,11 +5,11 @@ from django.core.serializers import serialize
 def add_data_to_session_as_dict(request, name, session, key, value):
 
     if str(key) not in session:
-        session[str(key)] = serialize('json', [value])
         try:
+            session[str(key)] = serialize('json', [value])
             request.session[name] = json.dumps(session)
         except:
-            raise ValidationError(f"Could not add data to {session}.{name} with values {{{ {key}:{value} }}}")
+            raise ValidationError(f"Could not add data to {session}.{name} with values {{ {key}:{value} }}")
 
 def read_session_data(request, name, default={}):
         session_data = request.session.get(name, default) 
@@ -17,6 +17,6 @@ def read_session_data(request, name, default={}):
             try:
                 return json.loads(session_data)
             except:
-                raise ValidationError(f"Could not load session data for {name}")
+                raise ValidationError(f"Could not load session data for: {name}")
         else:
             return session_data
