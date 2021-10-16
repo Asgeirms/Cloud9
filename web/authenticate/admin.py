3756ca07290da5446c8c118525cdae6c16e4bd37
sigtheidiot <sigridge@stud.ntnu.no>
2021-10-16 15:02:32 +0200

    #43 - Added manytomanyfield between user and schedule

--- web/authenticate/admin.py
+++ web/authenticate/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('swiped_events',)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
